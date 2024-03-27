080053e47ea1534e369486f50d6a1dd124b6b116
sonal chindage <sonalchindage@gmail.com>
2024-03-27 22:24:25 +0530

    completed step -04

--- tests/index.test.js
+++ tests/index.test.js
@@ -18,13 +18,27 @@
 
 // tests/index.test.js
 
-const parseQuery = require("../src/queryParser");
+// const parseQuery = require("../src/queryParser");
 
-test("Parse SQL Query", () => {
+// test("Parse SQL Query", () => {
+//   const query = "SELECT id, name FROM sample";
+//   const parsed = parseQuery(query);
+//   expect(parsed).toEqual({
+//     fields: ["id", "name"],
+//     table: "sample",
+//   });
+// // });
+
+// tests/index.test.js
+
+const executeSELECTQuery = require("../src/index");
+
+test("Execute SQL Query", async () => {
   const query = "SELECT id, name FROM sample";
-  const parsed = parseQuery(query);
-  expect(parsed).toEqual({
-    fields: ["id", "name"],
-    table: "sample",
-  });
+  const result = await executeSELECTQuery(query);
+  expect(result.length).toBeGreaterThan(0);
+  expect(result[0]).toHaveProperty("id");
+  expect(result[0]).toHaveProperty("name");
+  expect(result[0]).not.toHaveProperty("age");
+  expect(result[0]).toEqual({ id: "1", name: "John" });
 });
