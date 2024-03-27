9ad25c5ae6cacbc8614cfb667e6aed40c5d03205
sonal chindage <sonalchindage@gmail.com>
2024-03-27 22:21:21 +0530

    completed step -03

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,12 +6,25 @@
 
 // tests/index.test.js
 
-const readCSV = require("../src/csvReader");
+// const readCSV = require("../src/csvReader");
 
-test("Read CSV File", async () => {
-  const data = await readCSV("./sample.csv");
-  expect(data.length).toBeGreaterThan(0);
-  expect(data.length).toBe(3);
-  expect(data[0].name).toBe("John");
-  expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
+// test("Read CSV File", async () => {
+//   const data = await readCSV("./sample.csv");
+//   expect(data.length).toBeGreaterThan(0);
+//   expect(data.length).toBe(3);
+//   expect(data[0].name).toBe("John");
+//   expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
+// });
+
+// tests/index.test.js
+
+const parseQuery = require("../src/queryParser");
+
+test("Parse SQL Query", () => {
+  const query = "SELECT id, name FROM sample";
+  const parsed = parseQuery(query);
+  expect(parsed).toEqual({
+    fields: ["id", "name"],
+    table: "sample",
+  });
 });
